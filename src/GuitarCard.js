866ff6e2e81e0b6c66519c8ac1b2888212753a1e
github username <github email address>
2020-11-09 20:57:17 -0500

    added state and guitar detaails view

--- src/GuitarCard.js
+++ src/GuitarCard.js
@@ -2,12 +2,12 @@ import React from "react"
 import {Card, CardImg} from "react-bootstrap"
 
 const GuitarCard = (props) => {
-
+// console.log(props)
   // const showDetails = (guitar) => {
   //   console.log(`${guitar.name} was clicked`)
   // }
   return (
- <Card style={{ width: '10rem'}} className="box">
+ <Card onClick={()=>props.handleDetailsClick(props.guitar)} style={{ width: '10rem'}} className="box">
    <CardImg src={props.guitar.mainImage}></CardImg>
    <Card.Title>{props.guitar.name}</Card.Title>
    <Card.Body>Guitar Description</Card.Body>
--- src/GuitarContainer.js
+++ src/GuitarContainer.js
@@ -2,13 +2,15 @@ import React from "react"
 import GuitarCard from "./GuitarCard"
 import {Container} from 'react-bootstrap'
 import * as GuitarFunctions from './DataGrabber'
+import GuitarDetailsContainer from '../src/components/GuitarDetailsContainer'
 
 let guitars = GuitarFunctions.getAllGuitars()
 
 class GuitarContainer extends React.Component {
 
   state = {
-    guitars: []
+    guitars: [],
+    selectedGuitar: null
   }
   
   componentDidMount() {
@@ -23,14 +25,23 @@ class GuitarContainer extends React.Component {
    return guitars.map(guitar => 
     <GuitarCard
       guitar={guitar}
+      handleDetailsClick={this.handleDetailsClick}
     />
-    )
+   )
+  }
+
+  handleDetailsClick = (guitar) => {
+    this.setState({
+      selectedGuitar: guitar
+    })
   }
   
   render() {
+    console.log(this.state)
     return (
       <Container className="guitar-container">
       {this.renderGuitarCards()}
+      {this.state.selectedGuitar ? <GuitarDetailsContainer selectedGuitar={this.state.selectedGuitar}/> : null}
       </Container>
     )
   }
