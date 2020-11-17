38c55841b26461de6a2168c4ef555d211f5481e3
github username <github email address>
2020-11-17 18:48:40 -0500

    video test

--- src/Bio.js
+++ src/Bio.js
@@ -1,8 +1,9 @@
 import React from "react"
-
+import BioVideo from './components/BioVideo'
 const Bio = () => {
   return (
     <div className="bio-container">
+      <BioVideo/>
       <h3 className="light-text">As a one man shop, I dedicate ALL of my work to my own design custom guitars. At this time, I am no longer doing any repairs. If you'd like to get on the waiting list for a Corelli or Leopardi guitar, please reach out and we can discuss timeline and options. I look forward to hearing from you. I require a 50% deposit prior to beginning any work on a custom project.</h3>  
     </div>
   )
---
+++ src/components/BioVideo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactPlayer from 'react-player'
+
+const BioVideo = () => {
+  
+  return(
+    <div>
+      <ReactPlayer url="https://www.youtube.com/watch?v=9boMnm5X9ak"/>
+    </div>
+  )
+}
+
+export default BioVideo
