fcb9a2ed7a084397056a84a672f955dbbb90c35a
github username <github email address>
2020-11-18 20:11:09 -0500

    added video to guitar description. Not final

--- src/components/GuitarDetailsContainer.js
+++ src/components/GuitarDetailsContainer.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import ReactPlayer from 'react-player'
+import {Col, Row} from 'react-bootstrap'
 
 const GuitarDetailsContainer = (props) => {
   return (
     <div className="details-container">
-      <h1>Description: {props.selectedGuitar.description}</h1>
+      <Col>
+        <ReactPlayer url="https://youtu.be/uEnGcLJXHH0"/>
+      </Col>
+      <Col><h1>Description: {props.selectedGuitar.description}</h1></Col>
     </div>
   )
 }
