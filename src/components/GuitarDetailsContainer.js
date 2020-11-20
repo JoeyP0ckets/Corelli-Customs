14ce9fe9702f7a2f702d026e279173253ccb56e5
github username <github email address>
2020-11-20 10:19:01 -0500

    shows image of guitar if no url

--- src/components/GuitarDetailsContainer.js
+++ src/components/GuitarDetailsContainer.js
@@ -6,7 +6,7 @@ const GuitarDetailsContainer = (props) => {
   return (
     <div className="details-container">
       <Col>
-        <ReactPlayer url={props.selectedGuitar.url} alt={props.selectedGuitar.mainImage}/>
+        {props.selectedGuitar.url ? <ReactPlayer url={props.selectedGuitar.url}/> : <img src={props.selectedGuitar.mainImage}/>}
       </Col>
       <Col><h1>Description: {props.selectedGuitar.description}</h1></Col>
     </div>
