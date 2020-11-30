dc3e406037f908a97523f725b97eda5da18dd851
github username <github email address>
2020-11-29 19:17:13 -0600

    added demo videos

---
+++ src/components/DemoVids.js
@@ -0,0 +1,12 @@
+import React from "react"
+import ReactPlayer from "react-player"
+
+const DemoVids = () => {
+  return(
+    <div>
+      <ReactPlayer url= "https://youtu.be/yj3q8Aqzxik"/>
+    </div>
+  )
+}
+
+export default DemoVids
